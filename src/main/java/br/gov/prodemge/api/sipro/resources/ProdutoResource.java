539ed13800d3cebbefb771b9a742e8ede205433c
Lucas <lucas.kuririn@gmail.com>
2019-07-15 22:39:27 -0300

    swagger

--- src/main/java/br/gov/prodemge/api/sipro/resources/ProdutoResource.java
+++ src/main/java/br/gov/prodemge/api/sipro/resources/ProdutoResource.java
@@ -1,9 +1,9 @@
 package br.gov.prodemge.api.sipro.resources;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,20 +13,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.gov.prodemge.api.sipro.model.Produto;
 import br.gov.prodemge.api.sipro.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/api")
+@Api(value="API Rest Produtos")
+@CrossOrigin(origins="*" )
 public class ProdutoResource {
 	@Autowired
 	ProdutoRepository produtoRepository;
 
 	@GetMapping("/produtos")
+	@ApiOperation(value="Retorna uma lista de produtos")
 	public List<Produto> listaProdutos() {
 		return produtoRepository.findAll();
 	}
 
 	@GetMapping("/produto/{id}")
-	public Produto listaProdutoPorId(@PathVariable(value = "id") long id) {
+	@ApiOperation(value="Retorna um unico Produto")
+	public Produto getProdutoPorId(@PathVariable(value = "id") long id) {
 		// Produto produto = null;
 
 		// Optional<Produto> optional = produtoRepository.findById(id);
